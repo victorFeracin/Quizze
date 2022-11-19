1d53d76002110cd5fa7f3247445f2f4ff7bf64f2
Victor Hugo Feracin <victorhugo.feracin@gmail.com>
2022-11-19 18:08:59 -0300

    Creating backend structure and connecting to db

---
+++ Backend/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const PORT = 3000
+const cors = require ('cors')
+// const routes = require ('./src/routes')
+const db = require('./src/database')
+// const handleError = require ('./src/middlewares/handleError')
+
+const app = express()
+app.use (cors())
+
+db.conectarBanco()
+
+app.use(express.json())
+// app.use(routes)
+// app.use (handleError)
+
+
+app.listen(PORT, () => console.log('Server running on port '+ PORT))
